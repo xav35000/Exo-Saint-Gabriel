2deab6f05d892de23e6acc13fa2dd4519736a966
blacyre <95582771+xav35000@users.noreply.github.com>
2022-09-29 11:25:27 +0000

    First release pre-alpha

--- exo2.py
+++ exo2.py
@@ -1,28 +1,37 @@
 from colorama import Fore, Back, init, deinit
 import replit
-def ex2():
-  import replit
-  replit.clear()
-  print (" __      __        _       _     _           ")
-  print (" \ \    / /       (_)     | |   | |          ")
-  print ("  \ \  / /_ _ _ __ _  __ _| |__ | | ___  ___ ")
-  print ("   \ \/ / _` | '__| |/ _` | '_ \| |/ _ \/ __|")
-  print ("    \  / (_| | |  | | (_| | |_) | |  __/\__ |")
-  print ("     \/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/")    
-  print("Maintenant, nous allons voir les variable.\n")
-  print("Une variable sert à contenir quelque chose (des mots, des chiffres ...)")
-  print("Ex:")
-  print(Fore.GREEN + "chats = 2")
-  print(Fore.WHITE + "print('chats')")
-  print(Fore.WHITE + "Maintenant, à ton tour. Crée une variable appelée note, donne lui la valeur que tu veux")
-  var = input("")
-  rep = "note = "
-  while b == 0:
-    if var.find(rep):
-    print("bravo passons a la seconde partie du cour.\n maintenant, nous allons integrer une variable dans un print. pour cela, nous allons creer un print:\nprint ('Bonjour')\nNous allons ensuite ajouter un nom derriere le print.\nnom = 'Jean'\nprint ('Bonjour ' + nom)\nNous aurons donc une sortie du genre :\nBonjour Jean")
-    a = input()
-    b = 1
-    break
 
-    else:
-      print("Faux, Recommence")
+
+def ex2():
+    import replit
+    replit.clear()
+    print(" __      __        _       _     _           ")
+    print(" \ \    / /       (_)     | |   | |          ")
+    print("  \ \  / /_ _ _ __ _  __ _| |__ | | ___  ___ ")
+    print("   \ \/ / _` | '__| |/ _` | '_ \| |/ _ \/ __|")
+    print("    \  / (_| | |  | | (_| | |_) | |  __/\__ |")
+    print("     \/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/")
+    print("Maintenant, nous allons voir les variable.\n")
+    print(
+        "Une variable sert à contenir quelque chose (des mots, des chiffres ...)"
+    )
+    print("Ex:")
+    print(Fore.GREEN + "chats = 2")
+    print(Fore.WHITE + "print('chats')")
+    print(
+        Fore.WHITE +
+        "Maintenant, à ton tour. Crée une variable appelée note, donne lui la valeur que tu veux"
+    )
+    var = input("")
+    rep = "note = "
+    b = 0
+    while b == 0:
+        if var.find(rep):
+            print(
+                "bravo passons a la seconde partie du cour.\n maintenant, nous allons integrer une variable dans un print. pour cela, nous allons creer un print:\nprint ('Bonjour')\nNous allons ensuite ajouter un nom derriere le print.\nnom = 'Jean'\nprint ('Bonjour ' + nom)\nNous aurons donc une sortie du genre :\nBonjour Jean"
+            )
+            a = input()
+            b = 1
+            break
+        else:
+            print("Faux, Recommence")
--- exo3.py
+++ exo3.py
@@ -1,21 +1,26 @@
 from colorama import Fore, init, deinit
+
+
 def ex3():
-  import replit
-  replit.clear()
-  print (Fore.CYAN +   "  _____  __      ______ _          ")
-  print (Fore.CYAN +   " |_   _|/ _|    |  ____| |         ")
-  print (Fore.CYAN +   "   | | | |_     | |__  | |___  ___ ")
-  print (Fore.CYAN +   "   | | |  _|    |  __| | / __|/ _ |")
-  print (Fore.CYAN +   "  _| |_| |_ _ _ | |____| \__ \  __/")
-  print (Fore.CYAN +   " |_____|_(_|_|_)|______|_|___/\___|")
-         
-  print(Fore.WHITE + "\nIci, nous allons voir les condition 'if ... else'\nC'est une fonction qui vérifie une condition.\n Il y a trois éléments dans une boucle 'if ... else:   If, qui vérifie la condition    elif, qui, si la 1ére condition n'est pas vérifiée, en propose une deuxiéme      et else qui à la différence de elif, ne propose aucune deuxiéme condition et termine la boucle.\nOpérateurs:\n==, est égal à\n!=, est différent de\n<, plus petit que\n>, plus grand que\n<=, supérieur ou égal à\n>=, inférieur ou égal à\nStructure:\nscore = input('Quel était ta derniére note ?  (sur 20)  '')\nscore = int(score)\nif score <= 20:\n  score = str(score)\n  print('Ta note est: ' + score + '/20')\nelse:\n  print('La note entrée n'est pas valide')" )
-  score = input("Quel était ta derniére note ? (sur 20) ")
-  print("\nVoici le résultat du code ci-dessus:\n")
-  score = int(score)
-  if score <= 20:
-    score = str(score)
-    print("Ta note est: " + score + "/20")
-    
-  else:
-    print("La note entrée n'est pas valide")
+    import replit
+    replit.clear()
+    print(Fore.CYAN + "  _____  __      ______ _          ")
+    print(Fore.CYAN + " |_   _|/ _|    |  ____| |         ")
+    print(Fore.CYAN + "   | | | |_     | |__  | |___  ___ ")
+    print(Fore.CYAN + "   | | |  _|    |  __| | / __|/ _ |")
+    print(Fore.CYAN + "  _| |_| |_ _ _ | |____| \__ \  __/")
+    print(Fore.CYAN + " |_____|_(_|_|_)|______|_|___/\___|")
+
+    print(
+        Fore.WHITE +
+        "\nIci, nous allons voir les condition 'if ... else'\nC'est une fonction qui vérifie une condition.\n Il y a trois éléments dans une boucle 'if ... else:   If, qui vérifie la condition    elif, qui, si la 1ére condition n'est pas vérifiée, en propose une deuxiéme      et else qui à la différence de elif, ne propose aucune deuxiéme condition et termine la boucle.\nOpérateurs:\n==, est égal à\n!=, est différent de\n<, plus petit que\n>, plus grand que\n<=, supérieur ou égal à\n>=, inférieur ou égal à\nStructure:\nscore = input('Quel était ta derniére note ?  (sur 20)  '')\nscore = int(score)\nif score <= 20:\n  score = str(score)\n  print('Ta note est: ' + score + '/20')\nelse:\n  print('La note entrée n'est pas valide')"
+    )
+    score = input("Quel était ta derniére note ? (sur 20) ")
+    print("\nVoici le résultat du code ci-dessus:\n")
+    score = int(score)
+    if score <= 20:
+        score = str(score)
+        print("Ta note est: " + score + "/20")
+
+    else:
+        print("La note entrée n'est pas valide")
